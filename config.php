084d445bb50e00ea4560a58447a6e960a5a8bda2
soniyakadam1205 <90956287+soniyakadam1205@users.noreply.github.com>
2021-09-18 13:07:55 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 
-	$conn = pg_connect("host=ec2-52-72-125-94.compute-1.amazonaws.com dbname=dd3cj8cps2tohf user=dmorovidexkloz password=9471e5739d922967f063d440a180ab074978906727e0343d65905a31a5349be2");
+	$conn = pg_connect("host=host user=user password=password");
 	if(!$conn){
 		die("Could not connect to the database ".pg_last_error());
 	}
